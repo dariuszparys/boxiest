e4aa2a58886c76784dbc3159ebc54cdf849c06cf
Dariusz Parys <dparys@microsoft.com>
2020-12-22 11:15:36 +0100

    refactor: executing commands in own powershell instance

--- setup-dev-machine.ps1
+++ setup-dev-machine.ps1
@@ -13,23 +13,28 @@ function invokeScript {
     powershell -c "${boxiest_path}\scripts\$script"
 }
 
-Set-Location "${env:USERPROFILE}"
-If ( ! ( Test-Path "${PROFILE}" ) ) { 
-    New-Item -Force -ItemType File -Path "${PROFILE}"
-    Add-Content -Path "${PROFILE}" -Encoding UTF8 -Value "# Powershell Profile"; 
+function executeCommand {
+    param (
+        $command
+    )
+
+    powershell -c "${command}"
 }
 
+# Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope CurrentUser -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable --name allowGlobalConfirmation
+RefreshEnv
 
-refreshenv
+# Install git so we can clone the repository
+executeCommand "choco install git --params=""/GitAndUnixToolsOnPath /WindowsTerminal""; refreshenv"
+executeCommand "git clone https://github.com/dariuszparys/boxiest.git ${boxiest_path}"
 
-choco install git --params="/GitOnlyOnPath /WindowsTerminal /NoShellHereIntegration /SChannel"
-git clone https://github.com/dariuszparys/boxiest.git ${boxiest_path}
 Set-Location "${boxiest_path}"
 
-invokeScript "browsers.ps1"
-invokeScript "common-devtools.ps1"
-invokeScript "wsl-feature.ps1"
+#--- Setting up Windows ---
+invokeScript "browsers.ps1";
+invokeScript "common-devtools.ps1";
+invokeScript "wsl-feature.ps1";
 
 write-host "Please reboot..."
