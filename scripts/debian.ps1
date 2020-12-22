7f641de7837130d894cd87ba9bb9c5b90d30a2da
Dariusz Parys <dparys@microsoft.com>
2020-12-22 07:26:02 +0100

    refactor: package installation adjusted to WS2019

--- scripts/debian.ps1
+++ scripts/debian.ps1
@@ -1,5 +1,9 @@
 Invoke-WebRequest -Uri https://aka.ms/wsl-debian-gnulinux -OutFile ~/Debian.appx -UseBasicParsing
-Add-AppxPackage -Path ~/Debian.appx
+
+Rename-Item ~/Debian.appx ~/Debian.zip
+Expand-Archive ~/Debian.zip "${HOME}/Debian"
+$userenv = [System.Environment]::GetEnvironmentVariable("Path", "User")
+[System.Environment]::SetEnvironmentVariable("PATH", $userenv + ";${HOME}/Debian", "User")
 
 RefreshEnv
 Debian install --root
