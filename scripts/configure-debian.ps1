c4b24d5fa5e54da86aa99b82451fe9d9562f7ef2
Dariusz Parys <dparys@microsoft.com>
2020-12-23 10:36:29 +0100

    fix: calling now with powershell -c

--- scripts/configure-debian.ps1
+++ scripts/configure-debian.ps1
@@ -1,6 +1,6 @@
 # setting explicit path as refreshing env doesn't work
 
 $debian_path = "${HOME}\Debian"
-${debian_path}\debian install --root
-${debian_path}\debian run apt update
-${debian_path}\debian run apt upgrade -y
+powershell -c ${debian_path}\debian install --root
+powershell -c ${debian_path}\debian run apt update
+powershell -c ${debian_path}\debian run apt upgrade -y
